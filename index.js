1d87eb787dd566567bf782e0f50afbdb4b41fd13
tribeiro@auchan.fr <thomasribeiro123456@gmail.com>
2024-04-02 14:53:11 +0200

    feat

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const articleExample = [
-    { id: "1234", priceEur: 40, weightKg: 0.3, quantity: 2 },
-    { id: "5678", priceEur: 20, weightKg: 0.1, quantity: 5 },
+    { id: "1234", priceEur: 20, weightKg: 0.3, quantity: 2 },
+    { id: "5678", priceEur: 20, weightKg: 0.1, quantity: 2 },
 ];
 
 function getShippingCost(articles){
     // return number
     let totalWeight = 0;
+    let totalPrice = 0;
 
     articles.forEach(article => {
         totalWeight += article.weightKg * article.quantity;
     });
+    let shipping = totalWeight * 10;
+    articles.forEach(article => {
+        totalPrice += article.priceEur * article.quantity;
+    })
 
-    const shipping = totalWeight * 10;
-
+    if(totalPrice>=100){
+        shipping = 0
+    }
     return shipping;
 }
 
