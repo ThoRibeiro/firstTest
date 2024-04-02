aa72785200bd204df4a2058d3ed55de40f17b843
tribeiro@auchan.fr <thomasribeiro123456@gmail.com>
2024-04-02 15:23:32 +0200

    feat: install bun and jest

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-const articleExample = [
-    { id: "1234", priceEur: 20, weightKg: 0.3, quantity: 2, specialShippingCost : 10 },
-    { id: "5678", priceEur: 20, weightKg: 12, quantity: 2 },
-];
+    const articleExample = [
+        { id: "1234", priceEur: 20, weightKg: 0.3, quantity: 2, specialShippingCost : 10 },
+        { id: "5678", priceEur: 20, weightKg: 0.1, quantity: 2 },
+    ];
 
-function getShippingCost(articles){
-    // return number
-    let totalWeight = 0;
-    let totalPrice = 0;
-    let shipping = 0;
+    function getShippingCost(articles){
+        // return number
+        let totalWeight = 0;
+        let totalPrice = 0;
+        let shipping = 0;
 
-    articles.forEach(article => {
-        totalPrice += article.priceEur * article.quantity;
+        articles.forEach(article => {
+            totalPrice += article.priceEur * article.quantity;
 
-        if (article.specialShippingCost !== undefined) {
-            shipping += article.specialShippingCost * article.quantity;
+            if (article.specialShippingCost !== undefined) {
+                shipping += article.specialShippingCost * article.quantity;
+            }
+            if(article.specialShippingCost === undefined) {
+                totalWeight += article.weightKg * article.quantity;
+            }
+        });
+
+        if (articles.specialShippingCost === undefined) {
+            shipping += totalWeight * 10;
         }
-        if(article.specialShippingCost === undefined) {
-            totalWeight += article.weightKg * article.quantity;
+        if(totalPrice >= 100){
+            shipping = 0;
         }
-    });
 
-    if (articles.specialShippingCost === undefined) {
-        console.log(totalWeight)
-        shipping += totalWeight * 10;
-    }
-    if(totalPrice >= 100){
-        shipping = 0;
+        return shipping;
     }
 
-    return shipping;
-}
-
-const getShipping = getShippingCost(articleExample);
-console.log("Frais de port :", getShipping, "euros");
+    const getShipping = getShippingCost(articleExample);
+    console.log("Frais de port :", getShipping, "€");
