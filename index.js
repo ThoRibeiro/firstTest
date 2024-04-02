3024f50ae6f8d6020b8d3f81e3e18260443b6a52
tribeiro@auchan.fr <thomasribeiro123456@gmail.com>
2024-04-02 15:12:48 +0200

    Exercice 3 : des articles peuvent avoir des frais de port spécifiques (specialShippingCost) : ne pas les inclure dans le poids total, mais ajouter leurs frais de port spécifiques aux frais de ports finaux

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const articleExample = [
-    { id: "1234", priceEur: 20, weightKg: 0.3, quantity: 2 },
-    { id: "5678", priceEur: 20, weightKg: 0.1, quantity: 2 },
+    { id: "1234", priceEur: 20, weightKg: 0.3, quantity: 2, specialShippingCost : 10 },
+    { id: "5678", priceEur: 20, weightKg: 12, quantity: 2 },
 ];
 
 function getShippingCost(articles){
     // return number
     let totalWeight = 0;
     let totalPrice = 0;
+    let shipping = 0;
 
-    articles.forEach(article => {
-        totalWeight += article.weightKg * article.quantity;
-    });
-    let shipping = totalWeight * 10;
     articles.forEach(article => {
         totalPrice += article.priceEur * article.quantity;
-    })
 
-    if(totalPrice>=100){
-        shipping = 0
+        if (article.specialShippingCost !== undefined) {
+            shipping += article.specialShippingCost * article.quantity;
+        }
+        if(article.specialShippingCost === undefined) {
+            totalWeight += article.weightKg * article.quantity;
+        }
+    });
+
+    if (articles.specialShippingCost === undefined) {
+        console.log(totalWeight)
+        shipping += totalWeight * 10;
     }
+    if(totalPrice >= 100){
+        shipping = 0;
+    }
+
     return shipping;
 }
 
-const getShipping = getShippingCost(articleExample)
+const getShipping = getShippingCost(articleExample);
 console.log("Frais de port :", getShipping, "euros");
