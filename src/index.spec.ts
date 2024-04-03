7061da176652d8a8e4978364f1266c0ad6f25421
tribeiro@auchan.fr <thomasribeiro123456@gmail.com>
2024-04-03 11:06:15 +0200

    test: add test with getOrderCost

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,4 @@
-import { getShippingCost } from "./index";
+import {getOrderCost, getShippingCost} from "./index";
 
 describe("getShippingCost", () => {
     describe("if total article price greater than or equal to 100", () => {
@@ -23,17 +23,27 @@ describe("getShippingCost", () => {
         it("returns 10 euros per kilogram of total weight, excluding articles with special shipping, whose amount is added to total", () => {
             expect(
                 getShippingCost([
-                    {
-                        id: "1234",
-                        priceEur: 4,
-                        weightKg: 0.3,
-                        quantity: 2,
-                        specialShippingCost: 8,
-                    },
+                    { id: "1234", priceEur: 4, weightKg: 0.3, quantity: 2, specialShippingCost: 8},
                     { id: "5678", priceEur: 2, weightKg: 0.1, quantity: 5 },
                     { id: "5678", priceEur: 2, weightKg: 0.1, quantity: 1 },
                 ])
             ).toEqual(2 * 8 + 6 * 0.1 * 10); // 22
+        });
+    });
+});
+
+describe("getOrderCost", () => {
+    describe("if total article price less than 100", () => {
+        it("returns total cost without shipping, shipping cost, and total cost with shipping", () => {
+            const result = getOrderCost([
+                { id: "1234", priceEur: 4, weightKg: 0.3, quantity: 2, specialShippingCost: 8,},
+                { id: "5678", priceEur: 2, weightKg: 0.1, quantity: 5 },
+                { id: "5678", priceEur: 2, weightKg: 0.1, quantity: 1 },
+            ]);
+
+            expect(result.withoutShipping).toEqual(4 * 2 + 2 * 5 + 2);
+            expect(result.shipping).toEqual(2 * 8 + 6 * 0.1 * 10);
+            expect(result.withShipping).toEqual(4 * 2 + 2 * 5 + 2 + (2 * 8 + 6 * 0.1 * 10));
         });
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -22,4 +22,19 @@ function getShippingCost(articles: Article[]) {
         );
 }
 
-export { getShippingCost };
+// retourne le total de la commande
+function getOrderCost(articles: Article[]): { withoutShipping: number, shipping: number, withShipping: number } {
+    const totalWithoutShipping = articles.reduce(
+        (total, article) => total + article.priceEur * article.quantity,
+        0
+    );
+    const shippingCost = getShippingCost(articles);
+    const totalWithShipping = totalWithoutShipping + shippingCost;
+
+    return {
+        withoutShipping: totalWithoutShipping,
+        shipping: shippingCost,
+        withShipping: totalWithShipping
+    };
+}
+export { getShippingCost, getOrderCost};
